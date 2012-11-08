5ff4b6a170b92cdccd0dcc410bb01fb5d9a9e77f
David J. Kordsmeier <dkords@gmail.com>
2012-11-07 22:46:22 -0800

    Add url to update smileplug script.

--- spdist/update-smileplug.sh
+++ spdist/update-smileplug.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 version=0.0.1
 echo Running update-smilpluug.sh $version 
+
+# 
+# Don't EVER CHANGE THIS URL
+#
+curl https://raw.github.com/SMILEConsortium/smileplug-bsp/master/spdist/update-smileplug.sh
